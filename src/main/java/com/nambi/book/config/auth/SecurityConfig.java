d69472ccd60e6834f57b663ae2aa17ba65fe9a18
sleepygloa <sleepygloa@gmail.com>
2021-01-12 17:30:22 +0900

    url 규약 재수정

--- src/main/java/com/nambi/book/config/auth/SecurityConfig.java
+++ src/main/java/com/nambi/book/config/auth/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .logoutSuccessUrl("/")
                 .and()
                     .oauth2Login()
-                    .loginPage("/login")
                         .userInfoEndpoint()
                             .userService(customOAuth2UserService);
 
