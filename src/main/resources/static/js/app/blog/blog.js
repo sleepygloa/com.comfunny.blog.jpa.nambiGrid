799b7b70773acf9ddc7a32d4fb050d33f07517d2
sleepygloa <sleepygloa@gmail.com>
2021-01-22 17:55:10 +0900

    블로그 카테고리 구현, 수정버튼 누를때 활성화 되어야함.

--- src/main/resources/static/js/app/blog/blog.js
+++ src/main/resources/static/js/app/blog/blog.js
@@ -49,6 +49,37 @@ var blogJs = function(){
     		contentType : "application/json; charset=utf-8",
     		success     : function(result){
     		    console.log(result);
+
+    		    //카테고리A
+    		    var categoryA = new Array();
+    		    //카테고리C
+                var categoryB = new Array();
+    		    //카테고리C
+                var categoryC = new Array();
+
+                for(var i = 0; i < result.length; i++){
+                    categoryA.push(result[i].categoryA);
+                    categoryB.push(result[i].categoryB);
+                    categoryC.push(result[i].categoryC);
+                }
+
+
+    		    categoryA = categoryA.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+    		    categoryB = categoryB.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+    		    categoryC = categoryC.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+
+    		    //카테고리A
+    		    for(var i = 0; i < categoryA.length; i++){
+    		        $('#blogCategoryAList').append($('<option value="'+categoryA[i]+'">'));
+    		    }
+    		    //카테고리B
+    		    for(var i = 0; i < categoryB.length; i++){
+    		        $('#blogCategoryBList').append($('<option value="'+categoryB[i]+'">'));
+    		    }
+    		    //카테고리C
+    		    for(var i = 0; i < categoryC.length; i++){
+    		        $('#blogCategoryCList').append($('<option value="'+categoryC[i]+'">'));
+    		    }
     		}
         })
 	}
